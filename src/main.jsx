4550945756bebb0b61820c15675296d603edc438
saddamBD <saddam.ruet@gmail.com>
2024-09-23 07:47:18 +0600

    adding not found page

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Root from './routes/root';
+import ErrorPage from './error-page';
 import {
   createBrowserRouter,
   RouterProvider,
@@ -11,8 +12,10 @@ import './index.css'
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root/>
+    element: <Root/>,
+    errorElement: <ErrorPage/>
   },
+
 ]);
 
 createRoot(document.getElementById('root')).render(
