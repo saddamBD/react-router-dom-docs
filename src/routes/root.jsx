bca34690ea9c68b2e6fcd1b0e7599533baa1c175
saddamBD <saddam.ruet@gmail.com>
2024-09-23 08:08:41 +0600

    ensure client side redering

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet,Link} from "react-router-dom";
 
 export default function Root() {
   return (
@@ -31,11 +31,9 @@ export default function Root() {
         <nav>
           <ul>
             <li>
-              <a href={`/contacts/1`}>Your Name</a>
-            </li>
+              <Link to  ={`/contacts/1`}>Your Name</Link></li>
             <li>
-              <a href={`/contacts/2`}>Your Friend</a>
-            </li>
+              <Link to  ={`/contacts/2`}>Your Friend</Link></li>
           </ul>
         </nav>
       </div>
